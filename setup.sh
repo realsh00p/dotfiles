b38ce8bd6a5c7c68ae61d3ce3f0cfe31ddd7acec
realsh00p <@>
2021-04-17 12:41:26 +0200

    add setup script and improve structure

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+PWD="$(pwd)"
+ln -s "$PWD/i3" "$HOME/.config/i3"
+ln -s "$PWD/i3-polybar" "$HOME/.config/polybar"
+ln -s "$PWD/i3-rofi" "$HOME/.config/rofi"
+ln -s "$PWD/i3-networkmanager-dmenu" "$HOME/.config/networkmanager-dmenu"
+ln -s "$PWD/i3-networkmanager-dmenu-config/config.ini" "$HOME/.config/networkmanager-dmenu/config.ini"
+ln -s "$PWD/xfce4/terminal/colorschemes/gruvbox.theme" "$HOME/.config/xfce4/terminal/colorschemes/gruvbox.theme"
