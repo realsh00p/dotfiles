03f420ffa6eb286705ebb8a7776e9b19d0a1cc2d
realsh00p <@>
2021-04-18 21:45:11 +0200

    updates for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 set fish_greeting
-theme_gruvbox dark hard
+#theme_gruvbox dark hard
 
 if test -d ~/bin
    set -gx PATH ~/bin $PATH 
@@ -7,4 +7,8 @@ end
 
 if test -d ~/.local/bin
    set -gx PATH ~/.local/bin $PATH 
+end
+
+if test -d ~/.dotfiles/bin
+  set -gx PATH ~/.dotfiles/bin $PATH
 end
